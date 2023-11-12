0a3cfcd5e63eb014a103501a4fc1f535456a156b
meiranbot <87614073+meiranbot@users.noreply.github.com>
2023-11-12 19:35:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/meiranbot/VishaBotV12.git /VishaBotV12
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /VishaBotV12
 fi
-cd /PiroAutoFilterBot
+cd /VishaBotV12
 pip3 install -U -r requirements.txt
-echo "Starting PiroAutoFilterBot...."
+echo "Starting VishaBotV12...."
 python3 bot.py
